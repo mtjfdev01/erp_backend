a2e7e1a7d935090ab64a3f5f7dc657d191606005
razaoffi-droid <razaoffi@gmail.com>
2026-07-21 14:56:18 +0500

    update the task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,15 +43,15 @@ import { S3StorageModule } from "./utils/storage/s3-storage.module";
     S3StorageModule,
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      // url: process.env.DATABASE_URL,
-      // ssl: process.env.SSL === "true"
-      // ? { rejectUnauthorized: false }
-      // : false,     
+      // host: process.env.DB_HOST,
+      // port: parseInt(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      ssl: process.env.SSL === "true"
+      ? { rejectUnauthorized: false }
+      : false,     
       autoLoadEntities: true,
       synchronize: true,
       extra: {
