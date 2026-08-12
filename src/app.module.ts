665bfafa4b67f0a72d238c051043bc21a2faab1b
razaoffi-droid <razaoffi@gmail.com>
2026-08-12 23:39:54 +0500

    updated the ceo dashboard

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,15 +45,15 @@ import { CeoOfficeModule } from "./ceo_office/ceo-office.module";
     S3StorageModule,
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      // url: process.env.DATABASE_URL,
-      // ssl: process.env.SSL === "true"
-      // ? { rejectUnauthorized: false }
-      // : false,     
+      // host: process.env.DB_HOST,
+      // port: parseInt(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
+      ssl: process.env.SSL === "true"
+      ? { rejectUnauthorized: false }
+      : false,     
       autoLoadEntities: true,
       synchronize: true,
       extra: {
